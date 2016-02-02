0c8ea70a8419b6c13c0e24f815b5b5731c9a8822
benhigh22 <benjaming.high@gmail.com>
2016-02-02 12:51:13 -0500

    Second upload try

---
+++ pig_latin_project.py
@@ -0,0 +1,16 @@
+name = input("Enter your first name: ")
+pig = "ay"
+vowels = ["a", "e", "i", "o", "u"]
+word = name.lower()
+first = word[0]
+new_word = word[1:] + first + pig
+s = "s"
+new_vowel_name = word[1:] + s + pig
+if len(name) > 0 and name.isalpha():
+    print (new_word)
+elif first in vowels:
+    print (new_vowel_name)
+else:
+    print ("That's not a word")
+
+
